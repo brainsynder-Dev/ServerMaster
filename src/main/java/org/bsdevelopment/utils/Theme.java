0534ab2e28c42eb9b3ee1ca7d8de22c2ff5b6d88
brainsynder <briansnyder498@gmail.com>
2023-10-28 19:51:27 -0400

    Disabled all the other themes

--- src/main/java/org/bsdevelopment/utils/Theme.java
+++ src/main/java/org/bsdevelopment/utils/Theme.java
@@ -2,29 +2,25 @@ package org.bsdevelopment.utils;
 
 import com.formdev.flatlaf.FlatDarkLaf;
 import com.formdev.flatlaf.FlatLightLaf;
-import com.formdev.flatlaf.themes.FlatMacDarkLaf;
-import com.formdev.flatlaf.themes.FlatMacLightLaf;
 
 public enum Theme {
     LIGHT(FlatLightLaf::setup),
     DARK(FlatDarkLaf::setup),
+    UNKNOWN(() -> {});
 
-    ATOM_DARK(com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatAtomOneDarkIJTheme::setup),
-    ATOM_LIGHT(com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatAtomOneLightIJTheme::setup),
-    DARK_PURPLE(com.formdev.flatlaf.intellijthemes.FlatDarkPurpleIJTheme::setup),
-    DRACULA(com.formdev.flatlaf.intellijthemes.FlatDraculaIJTheme::setup),
-    GITHUB(com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatGitHubIJTheme::setup),
-    GITHUB_DARK(com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatGitHubDarkIJTheme::setup),
-    MAC_LIGHT(FlatMacLightLaf::setup),
-    MAC_DARK(FlatMacDarkLaf::setup),
-    NORD(com.formdev.flatlaf.intellijthemes.FlatNordIJTheme::setup),
-    OCEANIC(com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatMaterialOceanicIJTheme::setup),
-    PALENIGHT(com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatMaterialPalenightIJTheme::setup),
-    SOLARIZED_DARK(com.formdev.flatlaf.intellijthemes.FlatSolarizedDarkIJTheme::setup),
-    SOLARIZED_LIGHT(com.formdev.flatlaf.intellijthemes.FlatSolarizedLightIJTheme::setup),
-
-    UNKNOWN(() -> {
-    });
+//    ATOM_DARK(com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatAtomOneDarkIJTheme::setup),
+//    ATOM_LIGHT(com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatAtomOneLightIJTheme::setup),
+//    DARK_PURPLE(com.formdev.flatlaf.intellijthemes.FlatDarkPurpleIJTheme::setup),
+//    DRACULA(com.formdev.flatlaf.intellijthemes.FlatDraculaIJTheme::setup),
+//    GITHUB(com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatGitHubIJTheme::setup),
+//    GITHUB_DARK(com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatGitHubDarkIJTheme::setup),
+//    MAC_LIGHT(FlatMacLightLaf::setup),
+//    MAC_DARK(FlatMacDarkLaf::setup),
+//    NORD(com.formdev.flatlaf.intellijthemes.FlatNordIJTheme::setup),
+//    OCEANIC(com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatMaterialOceanicIJTheme::setup),
+//    PALENIGHT(com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatMaterialPalenightIJTheme::setup),
+//    SOLARIZED_DARK(com.formdev.flatlaf.intellijthemes.FlatSolarizedDarkIJTheme::setup),
+//    SOLARIZED_LIGHT(com.formdev.flatlaf.intellijthemes.FlatSolarizedLightIJTheme::setup);
 
     private final Runnable apply;
 
