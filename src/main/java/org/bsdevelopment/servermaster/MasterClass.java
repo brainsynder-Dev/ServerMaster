044facbff7fcea7b95f49cab3efeea8eefd80203
brainsynder <briansnyder498@gmail.com>
2024-03-31 00:19:12 -0400

    removed: not needed files

--- src/main/java/org/bsdevelopment/servermaster/MasterClass.java
+++
@@ -1,9 +0,0 @@
-package org.bsdevelopment.servermaster;
-
-import java.io.IOException;
-
-public class MasterClass {
-    public static void main(String[] args) throws IOException {
-        App.main(args);
-    }
-}
--- src/main/java/org/bsdevelopment/servermaster/views/data/JavaData.java
+++
@@ -1,3 +0,0 @@
-package org.bsdevelopment.servermaster.views.data;
-
-public record JavaData(boolean primary, String version, String path) {}
