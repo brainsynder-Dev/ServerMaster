4114459ef47464b6782f14b41dd092d4d2af6f04
brainsynder <briansnyder498@gmail.com>
2024-03-31 00:44:34 -0400

    added: a bunch of methods to the AppUtilities class

--- src/main/java/org/bsdevelopment/servermaster/utils/AppUtilities.java
+++ src/main/java/org/bsdevelopment/servermaster/utils/AppUtilities.java
@@ -1,21 +1,30 @@
 package org.bsdevelopment.servermaster.utils;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.dom.ThemeList;
+import com.vaadin.flow.theme.lumo.Lumo;
 import org.bsdevelopment.servermaster.App;
+import org.bsdevelopment.servermaster.AppConfig;
 
 import java.awt.*;
 import java.io.*;
-import java.net.URL;
 import java.net.URLConnection;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Consumer;
 import java.util.logging.Level;
 
 public class AppUtilities {
+    public static boolean CONSOLE_READY = false;
     private static final LinkedList<String> DELAYED_MESSAGES = new LinkedList<>();
 
     public static void logMessage (String message) {
@@ -25,13 +34,63 @@ public class AppUtilities {
         String text = ((level == null) ? "" : "["+level.getName()+"]: ")+message;
 
         System.out.println(text);
-        appendToUsernameFile(text);
+        appendToLogFile(text);
+    }
+
+    public static void logMessage(String prefix, String... messages) {
+        delayedLogMessage("");
+        String typePrefix = "@INFO";
+        if (prefix.startsWith("[WARNING]")) typePrefix = "@WARN";
+        if (prefix.startsWith("[ERROR]")) typePrefix = "@ERROR";
+        if (prefix.startsWith("[COMMAND]")) typePrefix = "@COMMAND";
+
+        delayedLogMessage(typePrefix+"@U1@U2 " + prefix);
+        delayedLogMessage(typePrefix+"@U3");
+
+        String spacer = typePrefix+"@U3";
+
+        for (String message : messages) {
+            delayedLogMessage(spacer + message);
+        }
+
+        delayedLogMessage(typePrefix+"@U3");
+        delayedLogMessage(typePrefix+"@U4@U5");
+        delayedLogMessage("");
+    }
+
+    public static void logMessage(LogPrefix prefix, String... messages) {
+        logMessage(prefix, "", Arrays.asList(messages));
+    }
+
+    public static void logMessage(LogPrefix prefix, String title, List<String> messages) {
+        delayedLogMessage("");
+        String typePrefix = "@INFO";
+        if (prefix == LogPrefix.WARNING) typePrefix = "@WARN";
+        if (prefix == LogPrefix.ERROR) typePrefix = "@ERROR";
+        if (prefix == LogPrefix.COMMAND) typePrefix = "@COMMAND";
+
+        delayedLogMessage(typePrefix+"@U1@U2 [" + prefix.name()+"] "+title);
+        delayedLogMessage(typePrefix+"@U3");
+
+        String spacer = typePrefix+"@U3";
+
+        for (String message : messages) {
+            delayedLogMessage(spacer + message);
+        }
+
+        delayedLogMessage(typePrefix+"@U3");
+        delayedLogMessage(typePrefix+"@U4@U5");
+        delayedLogMessage("");
     }
     public static void delayedLogMessage (String message) {
+        if (CONSOLE_READY) {
+            System.out.println(message);
+            return;
+        }
         DELAYED_MESSAGES.addLast(message);
     }
 
-    private static void appendToUsernameFile(String content)  {
+    private static void appendToLogFile(String content)  {
         try (FileWriter fw = new FileWriter(App.LOG_FILE, true); BufferedWriter bw = new BufferedWriter(fw)) {
             bw.write(content);
             bw.newLine();
@@ -67,22 +126,55 @@ public class AppUtilities {
         );
     }
 
-    public static void downloadFile(String fromUrl, File file) throws IOException {
-        if (file.exists()) file.delete();
-        file.createNewFile();
+    public static void downloadFile(URLConnection httpConnection, File file, Consumer<Integer> progressConsumer, Runnable finishTask) throws IOException {
+        new Thread(() -> {
+            if (file.exists()) file.delete();
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                AppUtilities.logMessage(AppUtilities.LogPrefix.ERROR, "Installer Issue", Arrays.asList(
+                        "An error has occurred when trying to create the '"+file.getName()+"' file",
+                        "Error: ",
+                        e.getMessage()
+                ));
+            }
 
-        URL url = new URL(fromUrl);
-        OutputStream out = new BufferedOutputStream(new FileOutputStream(file));
-        URLConnection conn = url.openConnection();
-        InputStream in = conn.getInputStream();
-        byte[] buffer = new byte[1024];
+            OutputStream outputStream = null;
+            InputStream inputStream = null;
+            try {
+                outputStream = new BufferedOutputStream(new FileOutputStream(file));
+                inputStream = httpConnection.getInputStream();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
 
-        int numRead;
-        while ((numRead = in.read(buffer)) != -1) {
-            out.write(buffer, 0, numRead);
-        }
-        in.close();
-        out.close();
+            byte[] buffer = new byte[1024];
+
+            long completeFileSize = httpConnection.getContentLength();
+            long downloadedFileSize = 0;
+
+            int dataLength;
+            try {
+                while ((dataLength = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, dataLength);
+
+                    downloadedFileSize += dataLength;
+                    int currentProgress = (int) (downloadedFileSize * 100 / completeFileSize);
+                    progressConsumer.accept(currentProgress);
+                }
+
+                inputStream.close();
+                outputStream.close();
+
+                finishTask.run();
+            }catch (Exception e) {
+                AppUtilities.logMessage(AppUtilities.LogPrefix.ERROR, "Installer Issue", Arrays.asList(
+                        "An error has occurred when trying to install the '"+file.getName()+"' file",
+                        "Error: ",
+                        e.getMessage()
+                ));
+            }
+        }).start();
     }
 
 
@@ -96,5 +188,43 @@ public class AppUtilities {
         layout.setAlignItems(FlexComponent.Alignment.CENTER);
         layout.setSpacing(false);
         return layout;
+    }
+
+    public static Span unicodeSpan (String className, String color) {
+        Span span = new Span ();
+        span.addClassName (className);
+        if (color != null) span.getStyle().set("color", color);
+        return span;
+    }
+
+    public static void sendNotification (String text, Notification.Position position, NotificationVariant variant) {
+        Notification notification = new Notification(text, 5000);
+        if (position != null) notification.setPosition(position);
+        if (variant != null) notification.addThemeVariants(variant);
+        notification.open();
+    }
+
+    public static void sendNotification (String text, Notification.Position position) {
+        sendNotification(text, position, null);
+    }
+
+    public static void sendNotification (String text, NotificationVariant variant) {
+        sendNotification(text, null, variant);
+    }
+
+    public enum LogPrefix {
+        INFO,
+        WARNING,
+        ERROR,
+        COMMAND
+    }
+
+    public static void updateTheme () {
+        UI ui = UI.getCurrent();
+        if (ui == null) return;
+
+        ThemeList themeList = ui.getElement().getThemeList();
+        themeList.clear();
+        if (!AppConfig.lightTheme) themeList.add(Lumo.DARK);
     }
 }
