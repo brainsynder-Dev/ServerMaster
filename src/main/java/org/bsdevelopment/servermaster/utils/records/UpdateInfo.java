e46e2a60199a5475f07fbd0bd63428fe943168c1
brainsynder <briansnyder498@gmail.com>
2025-07-19 21:04:01 -0400

    chore: removed all old code

--- src/main/java/org/bsdevelopment/servermaster/utils/records/UpdateInfo.java
+++
@@ -1,6 +0,0 @@
-package org.bsdevelopment.servermaster.utils.records;
-
-import org.bsdevelopment.servermaster.server.utils.Version;
-
-public record UpdateInfo(String releaseUrl, String title, Version version, String markdown) {
-}
--- src/main/java/org/bsdevelopment/servermaster/views/data/ServerTypeBuild.java
+++
@@ -1,8 +0,0 @@
-package org.bsdevelopment.servermaster.views.data;
-
-public record ServerTypeBuild(String build, String jar) {
-    @Override
-    public String toString() {
-        return build;
-    }
-}
