290478b8b42f53c8dc2c1c590d11a342bc6429be
brainsynder <briansnyder498@gmail.com>
2026-08-18 23:24:57 -0400

    feat: add logo tile text styling to enhance visual appearance

--- src/main/java/org/bsdevelopment/servermaster/ui/dialog/ServerInstallerDialog.java
+++ src/main/java/org/bsdevelopment/servermaster/ui/dialog/ServerInstallerDialog.java
@@ -209,6 +209,7 @@ public final class ServerInstallerDialog {
             tile.hoverProperty().addListener((obs, wasHover, isHover) -> nameLabel.setVisible(isHover));
         } else {
             tile.setText(displayName(project));
+            tile.getStyleClass().add("logo-tile-text");
         }
 
         return tile;
